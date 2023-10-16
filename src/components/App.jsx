9678fa8746f8f8b05dd6f8769b231097f2c54ee9
Mayte <mayte710@hotmail.com>
2023-10-16 09:49:52 +0100

    add new country

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,23 @@ const App = () => {
   const [countriesList, setcountriesList] = useState([]);
   const [nameSearch, setNameSearch] = useState('');
   const [continentSelect, setContinentSelect] = useState('');
+  const [newCountry, setNewcountry] = useState({
+    flag: '',
+    name: '',
+    capital: '',
+    continent: '',
+  });
+  const [error, setError] = useState('');
+
+  //petición a la api
 
   useEffect(() => {
     callToApi().then((response) => {
       setcountriesList(response);
     });
   }, []);
+
+  //handle
 
   const handleSelect = (ev) => {
     setContinentSelect(ev.target.value);
@@ -23,7 +34,34 @@ const App = () => {
   const handleInputSearch = (ev) => {
     setNameSearch(ev.target.value);
   };
+  const handleForm = (ev) => {
+    ev.preventDefault();
+  };
 
+  const handleClickAdd = () => {
+    if (
+      newCountry.name === '' ||
+      newCountry.flag === '' ||
+      newCountry.capital === '' ||
+      newCountry.continent === ''
+    ) {
+      setError('Debes rellenar todos los campos');
+    } else {
+      setcountriesList([...countriesList, newCountry]);
+      setError('');
+      setNewcountry({
+        flag: '',
+        name: '',
+        capital: '',
+        continent: '',
+      });
+    }
+  };
+  const handleInputAdd = (ev) => {
+    setNewcountry({ ...newCountry, [ev.target.id]: ev.target.value });
+  };
+
+  //render
   const renderCountries = () => {
     return countriesList
       .filter((eachCountry) =>
@@ -37,7 +75,7 @@ const App = () => {
 
       .map((eachCountry, index) => (
         <li key={index}>
-          <p>{eachCountry.flag}</p>
+          <i>{eachCountry.flag}</i>
           <p>{eachCountry.name}</p>
           <p>{eachCountry.capital}</p>
           <p>{eachCountry.continent}</p>
@@ -57,7 +95,7 @@ const App = () => {
       <main>
         <section>
           <h2>Filters</h2>
-          <form>
+          <form onSubmit={handleForm}>
             <label htmlFor="search">By Country: </label>
             <input
               type="search"
@@ -82,6 +120,45 @@ const App = () => {
               <option value="Oceania">Oceania</option>
             </select>
           </form>
+        </section>
+        <section>
+          <h2>Add Country</h2>
+          <form onSubmit={handleForm}>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={newCountry.name}
+              placeholder="Country Name"
+              onChange={handleInputAdd}
+            />
+            <input
+              type="text"
+              name="capital"
+              id="capital"
+              value={newCountry.capital}
+              placeholder="Capital"
+              onChange={handleInputAdd}
+            />
+            <input
+              type="text"
+              name="flag"
+              id="flag"
+              value={newCountry.flag}
+              placeholder="Flag Icon"
+              onChange={handleInputAdd}
+            />
+            <input
+              type="text"
+              name="continent"
+              id="continent"
+              value={newCountry.continent}
+              placeholder="Continent"
+              onChange={handleInputAdd}
+            />
+            <input type="submit" value="Add Country" onClick={handleClickAdd} />
+          </form>
+          <p>{error}</p>
         </section>
         <section>
           <ul>{renderCountries()}</ul>
